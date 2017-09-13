cb752757a34d0f07d80e2cee17149c02b60b75e3
Miriam Hendler <miriamthendler@gmail.com>
2017-09-12 21:58:05 -0700

    Initial Commit

---
+++ Dream HighTests/Dream_HighTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dream_HighTests.swift
+//  Dream HighTests
+//
+//  Created by Miriam Hendler on 9/12/17.
+//  Copyright © 2017 Miriam Hendler. All rights reserved.
+//
+
+import XCTest
+@testable import Dream_High
+
+class Dream_HighTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
